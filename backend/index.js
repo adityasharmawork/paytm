7876cf1897e15ec5377a0d163fa0b569f399fd59
Aditya Sharma <adityasharma2005123@gmail.com>
2025-06-21 16:41:41 +0530

    Made the index.js use the port number from the config file

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const rootRouter = require("./routes/index");
 const app = express();
-const PORT = 8080;
+const { PORT } = require('./config');
 const cors = require('cors');
 require('dotenv').config();
 
