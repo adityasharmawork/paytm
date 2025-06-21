b5fb6969c29f16811acbe0ec792e475ffdd6f1f3
Aditya Sharma <adityasharma2005123@gmail.com>
2025-06-21 15:14:45 +0530

    Added cors to all backend routes'

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const rootRouter = require("./routes/index");
 const app = express();
 const PORT = 8080;
+const cors = require('cors');
 
+app.use(cors);
 
 app.get("/api/v1", rootRouter);
 
