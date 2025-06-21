5f71be689977ef4f5114a297e8027815b3036a27
Aditya Sharma <adityasharma2005123@gmail.com>
2025-06-21 20:23:39 +0530

    Fixed typo error

--- backend/connectDB.js
+++ backend/connectDB.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 const MONGO_URI = require('./config');
 
-const connnectDB = async () => {
+const connectDB = async () => {
     mongoose.connect(MONGO_URI)
     .then(() => {
         console.log(`MongoDB connected successfully!`);
     });
 }
 
-module.exports = connnectDB;
+module.exports = connectDB;
