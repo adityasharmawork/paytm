452fa63b5952423a62b7e6a0a479867b828b3d9f
Aditya Sharma <adityasharma2005123@gmail.com>
2025-06-21 13:53:03 +0530

    Connected the router to /api/v1 routes

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,11 @@
 const express = require("express");
+const rootRouter = require("./routes");
 const app = express();
 const PORT = 8080;
+
+
+app.get("/api/v1", rootRouter);
+
 
 app.listen(PORT, () => {
     console.log(`Server listening on port: ${PORT}`)
