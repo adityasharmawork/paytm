5950be2b615379296788184045d9dfedeba0a97b
Aditya Sharma <adityasharma2005123@gmail.com>
2025-06-21 17:32:40 +0530

    Made the zod schema for validating req.body

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
-
+const zod = require('zod');
 const SECRET = require('../config');
+
+const signupBody = zod.object({
+    username: zod.string(),
+    firstName: zod.string(),
+    lastName: zod.string(),
+    password: zod.string()
+});
 
 router.post("/signup", async (req, res) => {
     const { username, firstName, lastName, password } = req.body;
