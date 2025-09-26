86f526b26caca5b46bb1df71bc3fa031d08049a3
Aditya Sharma <adityasharma2005123@gmail.com>
2025-09-26 20:21:56 +0530

    Added Authentication Middleware

---
+++ backend/middleware.js
@@ -0,0 +1,22 @@
+const { JWT_SECRET } = require("./config");
+const jwt = require("jsonwebtoken");
+
+const authMiddleware = (res, req, next) => {
+    const authHeader = req.headers.authorization;
+    if(!authHeader || !authHeader.startsWith('Bearer')) {
+        return res.status(403).json({});
+    }
+
+    const token = authHeader.split(" ")[1];
+
+    try {
+        const decoded = jwt.verify(token, JWT_SECRET);
+        req.userId = decoded.userId;
+    } catch(err) {
+        return res.status(403).json({});
+    }
+};
+
+module.exports = {
+    authMiddleware
+}
