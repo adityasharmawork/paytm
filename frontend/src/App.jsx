c17722a72e7cc1b5859e10025781520670f98394
Aditya Sharma <adityasharma2005123@gmail.com>
2025-11-05 22:45:10 +0530

    Made Heading Component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,16 @@
 function App() {
 
   return (
-    <div>
-        Hello world
-    </div>
+    <>
+      <BrowserRouter>
+        <Router>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/send" element={<SendMoney />} />
+        </Router>
+      </BrowserRouter>
+    </>
   )
 }
 
