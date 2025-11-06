746c0b4220b66dea8202963b40619cfc157a869a
Aditya Sharma <adityasharma2005123@gmail.com>
2025-11-07 00:04:02 +0530

    Added more components

---
+++ frontend/src/components/BottomWarning.jsx
@@ -0,0 +1,13 @@
+
+export default function BottomWarning({label, buttontext, to}) {
+  return (
+    <div>
+        <div>
+
+        </div>
+        <link>
+        
+        </link>
+    </div>
+  )
+}
