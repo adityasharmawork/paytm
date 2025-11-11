9e6af324fbcd5467e32f17c46747977acf4a7d70
Aditya Sharma <adityasharma2005123@gmail.com>
2025-11-11 23:29:10 +0530

    Added Balance Component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Signup from "./components/Signup"
 import Signin from "./components/Signin";
 import Appbar from "./components/Appbar";
+import Balance from "./components/Balance";
 
 function App() {
 
@@ -12,7 +13,7 @@ function App() {
           <Route path="/signup" element={<Signup />} />
           <Route path="/signin" element={<Signin />} />
           {/* <Route path="/dashboard" element={<Dashboard />} /> */}
-          <Route path="/dashboard" element={<Appbar />} />
+          <Route path="/dashboard" element={<Balance />} />
           {/* <Route path=  "/send" element={<SendMoney />} /> */}
         </Routes>
       </BrowserRouter>
