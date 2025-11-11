b7d4b7705688fe11b6ef0c473043045783944819
Aditya Sharma <adityasharma2005123@gmail.com>
2025-11-12 00:40:45 +0530

    Added Users Component

---
+++ frontend/src/components/Users.jsx
@@ -0,0 +1,23 @@
+
+export default function Users() {
+
+    const [users, setUsers] = useState([{
+        firstName: "Harkirat",
+        lastName: "Singh",
+        _id: 1
+    }]);
+
+  return (
+    <>
+        <div className="font-bold mt-6 text-lg">
+            Users
+        </div>
+        <div className="my-2">
+            <input type="text" placeholder="Search users..." className="w-full px-2 py-1 border rounded border-slate-200"></input>
+        </div>
+        <div>
+            {users.map(user => <User user={user} />)}
+        </div>
+    </>
+  )
+}
