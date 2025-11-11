193e36dd44c3b5a0846d5796d20a5189ec7fdb68
Aditya Sharma <adityasharma2005123@gmail.com>
2025-11-11 12:13:06 +0530

    Made BottomWarning Component

--- frontend/src/components/BottomWarning.jsx
+++ frontend/src/components/BottomWarning.jsx
@@ -1,12 +1,12 @@
 
 export default function BottomWarning({label, buttontext, to}) {
   return (
-    <div>
+    <div className="py-2 text-sm flex justify-center">
         <div>
-
+          {label}
         </div>
-        <link>
-        
+        <link className="pointer underline pl-1 cursor-pointer" to={to}>
+          {buttontext}
         </link>
     </div>
   )
