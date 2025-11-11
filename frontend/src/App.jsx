9f3eecc982ca6fd3c91c60a98940f59a83b3a958
Aditya Sharma <adityasharma2005123@gmail.com>
2025-11-11 23:23:55 +0530

    Added Appbar component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Signup from "./components/Signup"
 import Signin from "./components/Signin";
+import Appbar from "./components/Appbar";
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
           <Route path="/signup" element={<Signup />} />
           <Route path="/signin" element={<Signin />} />
           {/* <Route path="/dashboard" element={<Dashboard />} /> */}
+          <Route path="/dashboard" element={<Appbar />} />
           {/* <Route path=  "/send" element={<SendMoney />} /> */}
         </Routes>
       </BrowserRouter>
