6719b9085ed63921ed61cd7b444b0b6474a7e56b
Aditya Sharma <adityasharma2005123@gmail.com>
2025-11-11 22:57:14 +0530

    Added more components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Signup from "./components/Signup"
+import Signin from "./components/Signin";
 
 function App() {
 
@@ -8,9 +9,9 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/signup" element={<Signup />} />
-          {/* <Route path="/signin" element={<Signin />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/send" element={<SendMoney />} /> */}
+          <Route path="/signin" element={<Signin />} />
+          {/* <Route path="/dashboard" element={<Dashboard />} /> */}
+          {/* <Route path=  "/send" element={<SendMoney />} /> */}
         </Routes>
       </BrowserRouter>
     </>
--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -18,7 +18,7 @@ export default function Signup() {
                 <div className="pt-4">
                     <Button label={"Sign up"} />
                 </div>
-                <BottomWarning label={"Alreday have an account"} buttontext={"Sign in"} to={"/signin"} />
+                <BottomWarning label={"Already have an account?"} buttonText={"Sign in"} to={"/signin"} />
             </div>
         </div>
     </div>
