820516f2530cb124345d9bdb4e00ef8625ed9150
Aditya Sharma <adityasharma2005123@gmail.com>
2025-12-02 23:05:37 +0530

    Added Bulk User Call API

--- frontend/src/components/Users.jsx
+++ frontend/src/components/Users.jsx
@@ -1,11 +1,22 @@
+import { useEffect } from "react";
 
 export default function Users() {
 
-    const [users, setUsers] = useState([{
-        firstName: "Aditya",
-        lastName: "Sharma",
-        _id: 1
-    }]);
+    // const [users, setUsers] = useState([{
+    //     firstName: "Aditya",
+    //     lastName: "Sharma",
+    //     _id: 1
+    // }]);
+
+    const [users, setUsers] = useState([]);
+    const [filter, setFilter] = useState("");
+
+    useEffect(() => {
+        axios.get("http://localhost:3000/api/v1/user/bulk?filter=" + filter)
+            .then(response => {
+                setUsers(response.data.user)
+            });
+    }, [filter]);
 
   return (
     <>
